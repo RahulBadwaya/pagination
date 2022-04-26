deeaae9b18d22f788d585f753fd467308eb12d35
Rahul Badwaya <badwayarahul@gmail.com>
2022-04-26 18:37:48 +0530

    'change'

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
  import "./App.css"
  import { useState, useEffect } from "react";
+ const LIMIT = 16;
  const App =()=>{
    const [imgs , setImgs ] = useState([])
    const [data , setData] = useState([])
@@ -32,48 +33,13 @@
     setImgs(updatedImgs)
     console.log(start,end)
      }
-     const first=()=>{
-      const updatedImgs = data.filter(img=> (img.id >=1 && img.id<=16));
-      setImgs(updatedImgs)
-     }
-     const second=()=>{
-      const updatedImgs = data.filter(img=> (img.id >=17 && img.id<=32));
-      setImgs(updatedImgs)
-     }
-     const third=()=>{
-      const updatedImgs = data.filter(img=> (img.id >=33 && img.id<=48));
-      setImgs(updatedImgs)
-     }
-     const fourth=()=>{
-      const updatedImgs = data.filter(img=> (img.id >=49 && img.id<=64));
-      setImgs(updatedImgs)
-     }
-     const fifth=()=>{
-      const updatedImgs = data.filter(img=> (img.id >=65 && img.id<=80));
-      setImgs(updatedImgs)
-     }
-     const sixth=()=>{
-      const updatedImgs = data.filter(img=> (img.id >=81 && img.id<=96));
-      setImgs(updatedImgs)
-     }
-     const seven=()=>{
-      const updatedImgs = data.filter(img=> (img.id >=97 && img.id<=112));
-      setImgs(updatedImgs)
-     }
-     const eight=()=>{
-      const updatedImgs = data.filter(img=> (img.id >=113 && img.id<=128));
-      setImgs(updatedImgs)
-     }
-     const nine=()=>{
-      const updatedImgs = data.filter(img=> (img.id >=129 && img.id<=144));
-      setImgs(updatedImgs)
-     }
-     const ten=()=>{
-      const updatedImgs = data.filter(img=> (img.id >=145 && img.id<=160));
-      setImgs(updatedImgs)
+     const jump =(value)=>{
+       const newStart = LIMIT * value;
+       
+       const updatedImgs = data.filter(img=> (img.id >= (newStart + 1)  && img.id<= newStart + LIMIT));
+    setImgs(updatedImgs)
      }
     
-
   return <div className="mainContainer">
      {imgs.map((ob)=>{
          return <img key={ob.id} className="col-md-3 img" src ={ob.url} />
@@ -81,16 +47,11 @@
      })}
   <div align="center">
      <button className="btn-lg btn-info" onClick={prevPage}>Prev</button> &nbsp;&nbsp;&nbsp;
-     <button className="btn-lg btn-outline-info" onClick={first}>1</button>&nbsp;
-     <button className="btn-lg btn-outline-info" onClick={second}>2</button>&nbsp;
-     <button className="btn-lg btn-outline-info" onClick={third}>3</button>&nbsp;
-     <button className="btn-lg btn-outline-info" onClick={fourth}>4</button>&nbsp;
-     <button className="btn-lg btn-outline-info" onClick={fifth}>5</button>&nbsp;
-     <button className="btn-lg btn-outline-info" onClick={sixth}>6</button>&nbsp;
-     <button className="btn-lg btn-outline-info" onClick={seven}>7</button>&nbsp;
-     <button className="btn-lg btn-outline-info" onClick={eight}>8</button>&nbsp;
-     <button className="btn-lg btn-outline-info" onClick={nine}>9</button>&nbsp;
-     <button className="btn-lg btn-outline-info" onClick={ten}>10</button>&nbsp;&nbsp;&nbsp;
+     {imgs.map((ob,i)=>{
+       if(i<10)
+       return <button className="btn-lg btn-outline-info btn-bottom" onClick={() => jump(i)}>{i+1}</button>
+     })}
+     &nbsp;&nbsp;&nbsp;
      <button className="btn-lg btn-info" onClick={nextPage}>Next</button>
   </div>
 
